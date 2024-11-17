f365a6eb90110d371176a59e79e63dfb62015818
shameleon <john.mouaikel@gmail.com>
2024-11-17 23:39:17 +0100

    class CgxQuickFormatter tests 100%

--- Python_puzzles/hard/cgx-formatter/cgx_formatter.py
+++ Python_puzzles/hard/cgx-formatter/cgx_formatter.py
@@ -3,118 +3,135 @@ import math
 import re
 
 
-
-class Node:
-    """ stores elements """
-    def __init__(self, content, parent=None, element_type=None):
-        self.type = element_type
-        self.content = content
-        self.parent = parent
-        self.colon = False
-        self.children = []
-        self.depth = self.depth()
-        self.check_content()
-
-    def is_root(self):
-        return self.parent is None
-
-    def is_leaf(self):
-        return len(self.children)
-
-    def depth(self):
-        if self.parent == None:
-            return 0
-        else:
-            return self.parent.depth() + 1
-        
-    def check_content(self):
-        line = self.line
-        if line[-1] == ';':
-            self.colon = True
-            line = line[0:-1]
-        if self.line[0] == '(' and self.line[-1] == ')':
-            
-            for c in line:
-                c == '(':
-                    +=
-            
-        # rules = {'Bool_val': re.compile(r'(true|false){1}\;{0,1}'),
-        #          'Num_val': re.compile(r'\-?\d+(\.\d+)?'),
-        #          'Primitive': re.compile(r'\'(.[^\'])+\''),
-        #          'Key': re.compile(r'\'\w+\'='),
-        #          'Key-Value': re.compile(r'')}
-        # if self.type != None:
-        #     return
-        # for key, rule in rules.items():
-        #     m = re.fullmatch(rule, self.content)
-        #     m.span()
-    
-    def print_children(self):
-
-
 class CgxLexerParser:
-    def __init__(self, line):
-        self.line = line
-        self.root = []
-        self.nodes = []
-        self.lexer(self.root, line)
+    def __init__(self, cgx_line):
+        self.cgx_line = cgx_line
+        self.return_lines = []
+        prime = "'hello'= 'pedro'; 'key' = 4'"
+        print(self.find_primitive(prime), file=sys.stderr, flush=True)
 
-    def lexer(self, parent, line):
-        line = line.lstrip()
-        line = line.rstrip()
-        if len(line) == 0:
-            return
-        self.root.append(Node(line))
+        self._next_token(cgx_line, 0)
 
-    def print_tokens(self):
-        for root in self.root:
-            root.print_children()
+    def is_primitive(self, input_text):
+        pattern = re.compile(r'^\'.*\' *= *\'.*\'')
+        return pattern.match(input_text)
+
+    def is_boolean(self, input_text):
+        pattern = re.compile(r'^true|false', re.IGNORECASE)
+        return pattern.match(input_text)
+
+    def find_block(self, line):
+        flag = -1
+        for i in range(len(line)):
+            flag += line[i] == '('
+            if line[i] == ')' and flag == 0:
+                if i + 2 < len(line):
+                    i += line[i + 1] == ';'
+                    content = line[:i + 1]
+                    return [content, line[i+1:]]
+                else:
+                    return [line, ""]
+            flag -= line[i] == ')'
+        return []
+
+    def find_primitive(self, line):
+        pattern_key = re.compile(r'^\'.[\']*\' *=')
+        end = pattern_key.match(line)
+        # key_end = line[1:].find('\'') + 1
+        # key = line[:key_end]
+        # right_line = line[key_end:].lstrip()
+        return line[:end]
+
+
+    def _print_token(self, token: str, indent: int, next_elem: bool, elem_type='Default'):
+        str = (" " * 4 * indent) + token + next_elem * ";"
+        endline = '\n'
+        print(str, end=endline)
 
     def _next_token(self, line, indent):
-        """ lexer
-        3 types of elements : 'Block' 'Value' 'Primitive'"""
-        if len(line) == 0:
-            return
-        next_elem = (line[-1] == ';')
-        if next_elem:
-            line = line[0:-2]
-        many_values = re.search(r'(\'?\-?\w+\'?\;)+(\'?\-?\w+\'?)$', line)
-        if many_values:
-            found_values = re.search(r'\'?\-?\w+\'?;', line)
-            last = re.search(r'(\'?\-?\w+\'?)$', line)
-            if found_values and last:
-                values = re.finditer(r'\'?\-?\w+\'?;', line)
-                for value in values:
-                    self._print_token(value.group(), indent, False)
-                    #print(value.group(), file=sys.stderr, flush=True)
-                    #self._next_token(value.group(), indent)
-                #print(last.group(), file=sys.stderr, flush=True)
-                self._print_token(last.group(), indent, False)
-                #self._next_token(last.group(), indent)
-        elif line[0] == '(' and line[-1] == ')':
-            content = line[1: -1]
-            self._print_token("(", indent, next_elem)
-            self._next_token(line[1: -1], indent + 1)
-            self._print_token(")", indent, next_elem)
-        elif line[0] == '\'' and line[-1] == '\'':
-            self._print_token(line, indent, next_elem)
+        line = line.strip()
+        if line[0] == '(':
+            content, next_content = self.find_block(line)
+            self._print_token("(", indent, False)
+            self._next_token(content, indent + 1)
+            self._print_token(")", indent, False)
+            self._next_token(next_content, indent)
+        elif line[0] == '\'':
+            self.find_primitive(line)
         else:
-            print("VALUE", file=sys.stderr, flush=True)
-            bool_val = re.search(r'true|false', line)
-            num_val = re.search(r'^\-?\d+$', line)
-            if bool_val:
-                self._print_token(bool_val.group(), indent, next_elem)
-            elif num_val:
-                self._print_token(num_val.group(), indent, next_elem)
+            print(line)
 
+
+class CgxQuickFormatter:
+    def __init__(self, cgxline):
+        self.line  = self.strip_spaces(cgxline)
+        print(self.line, file=sys.stderr, flush=True)
+        print('-' * 80, file=sys.stderr, flush=True)
+        self.parse_line()
+
+    def parse_line(self):
+        result = ""
+        indent = 0
+        inside_string = False
+        new_line = True
+        previous = None
+        for c in self.line:
+            if inside_string:
+                inside_string =  not (c == '\'')
+                result += c
+            else:
+                if c == '(':
+                    if previous == '=':
+                        result += '\n' + (indent * ' ')
+                    result += (indent * new_line * ' ') + c + '\n'
+                    indent += 4
+                    new_line = True
+                elif c == ')':
+                    indent -= 4
+                    result += '\n' * (not new_line)
+                    result += (indent * ' ') + c
+                    new_line = False
+                elif c == '\'':
+                    result +=  (indent * new_line * ' ') + c
+                    inside_string = True
+                    new_line = False
+                elif c == ';':
+                    result += ';\n'
+                    new_line = True
+                elif c in ['\n', '\t', ' ']:
+                    new_line = True
+                else:
+                    result +=  (indent * new_line * ' ') + c
+                    new_line = False
+            previous = c
+        print(result)
+
+    def strip_spaces(self, line):
+        result = ""
+        can_strip_space = True
+        inside_string = False
+        for c in line:
+            if inside_string:
+                inside_string =  not (c == '\'')
+                result += c
+            else:
+                if c in ['\n', '\t', ' ']:
+                    continue
+                if c == '\'':
+                    inside_string = True
+                result += c
+        return result
+        
 
 def main():
     n = int(input())
     cgxline = ""
     for i in range(n):
         cgxline += input()
-    print(f'{cgxline}$', file=sys.stderr, flush=True)
-    parsing = CgxLexerParser(cgxline)
+    print(f'{cgxline}', file=sys.stderr, flush=True)
+    print('-' * 80, file=sys.stderr, flush=True)
+    cgx_quick = CgxQuickFormatter(cgxline)
+    print('-' * 80, file=sys.stderr, flush=True)
 
 
 if __name__ == "__main__":
