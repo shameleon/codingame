818a4f7da645ba18933ebf1445ef8c1e5f8ee9f8
shameleon <john.mouaikel@gmail.com>
2025-01-20 00:15:54 +0100

    Mayan calculation - medium difficulty

--- Python_puzzles/medium/mayan-calculation/mayan_calculation.py
+++ Python_puzzles/medium/mayan-calculation/mayan_calculation.py
@@ -1,82 +1,55 @@
 import sys
-import math
 
-"""
-progress 83%
-class Numeral:
+
+class NumeralSymbols:
     def __init__(self, l, h, numeral):
         self.l = l
         self.h = h
         self.numeral = numeral
+        self.base = 20
+        self.d = {}
+        for n in range(self.base):
+            self.d[n] = [numeral[i][n * l:(n + 1) * l] for i in range(h)]
 
-        for line in numeral:
-            for j in range(20):
-                k = j * self.l
-                line[k:k+4]
-"""
+    def _mayan_digit_to_int(self, mayan):
+        for key, value in self.d.items():
+            to_be_found = self.h
+            for i in range(self.h):
+                if mayan[i] != value[i]:
+                    break
+                to_be_found -= 1
+            if to_be_found == 0:
+                return key
+        return None
 
-
-class Numeral:
-    def __init__(self, l, h, numeral):
-        self.l = l
-        self.h = h
-        self.numeral = numeral
-        self.high = '....o...oo..ooo.oooo'
-        self.lows = '....____'
-
-    def numeral_to_dec(self, num):
+    def _int_to_mayan_digit(self, number):
+        if 0 <= number < self.base:
+            return self.d[number]
+        return None
+    
+    def mayan_to_decimal(self, mayan):
+        size = len(mayan) // self.h
         result = 0
-        sections = len(num) // self.h
-        for i in range(sections):
-            k = i * self.h
-            mayan = [num[i] for i in range(k, k + self.h)]
-            dec = self._mayan_to_dec(mayan)
-            print(i, mayan, dec, file=sys.stderr, flush=True)
-            if dec >= 0:
-                result += dec * pow(20, sections - i - 1)
-        print(result, file=sys.stderr, flush=True)
+        while size > 0:
+            mayan_digit = [mayan.pop(0) for i in range(self.h)]
+            nb = self._mayan_digit_to_int(mayan_digit)
+            result += nb * pow(20, size - 1)
+            size -= 1
         return result
 
-    def _mayan_to_dec(self, mayan):
-        pos = 0
-        for i in range(self.h):
-            pos += self.numeral[i].find(mayan[i])
-            if pos == 0:
-                return pos
-        pos = self.high.find(mayan[0])
-        if pos == -1:
-            return pos
-        nb = pos // 4
-        for i in range(1, self.h):
-            nb +=  5 * (self.lows.find(mayan[i]) // self.h)
-            if pos == -1:
-                return pos
-        return nb
-
-    def _dec_to_mayan(self, dec):
-        high = ['....', 'o...', 'oo..', 'ooo.', 'oooo']
-        lows = ['....', '____']
-        if dec == 0:
-            mayan = ['.oo.', 'o..o', '.oo.', '....']
-        else:
-            mayan = []
-            mayan.append(high[dec % 5])
-            for i in range(1, self.h):
-                mayan.append(lows[dec >= 5 * i])
-        return mayan
 
 
 class MayanOperation:
     def __init__(self, l, h, numeral):
-        self.num = Numeral(l, h, numeral)
+        self.num = NumeralSymbols(l, h, numeral)
 
     def calculate(self, operand1, operand2, operation):
-        self.nb1 = self.num.numeral_to_dec(operand1)
-        self.nb2 = self.num.numeral_to_dec(operand2)
+        self.nb1 = self.num.mayan_to_decimal(operand1)
+        self.nb2 = self.num.mayan_to_decimal(operand2)
         print(self.nb1, operation, self.nb2, file=sys.stderr, flush=True)
         result = self._operate(operation)
         self._result_to_mayan(result)
-
+    
     def _operate(self, operation):
         result = self.nb1
         if operation == '+':
@@ -88,17 +61,16 @@ class MayanOperation:
         elif operation == '/' and self.nb2 != 0:
             result //= self.nb2
         return result
-
+    
     def _result_to_mayan(self, result):
         print(result, file=sys.stderr, flush=True)
         if result == 0:
-            [print(line) for line in  self.num._dec_to_mayan(result)]
+            [print(line) for line in self.num._int_to_mayan_digit(0)]
         factors = self.factorize_to_base_20(result)
         for dec in factors:
-            numeral = self.num._dec_to_mayan(dec)
-            [print(line) for line in numeral]
-        #if res[-1] == 0:
-        #    [print(line) for line in self.num.dec_to_mayan(res[0])]
+            output = self.num._int_to_mayan_digit(dec)
+            for line in output:
+                print(line)
 
     @staticmethod
     def factorize_to_base_20(dec):
@@ -109,8 +81,8 @@ class MayanOperation:
         for n in range(p, -1, -1):
             factors.append(dec // pow(20, n))
             dec = dec % pow(20, n)
-        print("F", factors, file=sys.stderr, flush=True)
         return factors
+
 
 
 def main():
