afe29f3407b6438ab7d4385d27ec5cfcc0db53f9
shameleon <john.mouaikel@gmail.com>
2025-01-23 10:56:07 +0100

    stunning numbers passed 100% tests but 80% validators

--- Python_puzzles/easy/stunning-numbers/stunning-numbers.py
+++ Python_puzzles/easy/stunning-numbers/stunning-numbers.py
@@ -1,41 +1,61 @@
 import sys
 
 """
-
+    80% success - 2 validators not ok
 """
 
 class  StunningNumberSearch:
     def __init__(self, nb:str):
         self.nb = nb
         print(["false", "true"][check_if_stunning(nb)])
-        self.next_sn = self.get_next_stunnning_number(nb))
+        self.next_sn = self.get_next_stunnning_number(nb)
         print(self.next_sn)
 
     def get_next_stunnning_number(self, nb: str):
-        """" 99 87 100"""
-        flippable_digit = '012--56-89'
-        next_flippable = '0125556889'   
-        symetric = '012--59-86'
-        next_self_symetric = '018888888*'
+        flippable_digit    = '012--56-89'
+        next_flippable     = '0125556889'   
+        symetric           = '012--59-86'
         l = len(nb)
-        left = ['', nb[:l // 2]][l >= 2]
-        mid = ['', nb[l//2]][l % 2 == 1]
+        has_mid = (l % 2 == 1)
+        size = l // 2 + 1 * has_mid
+        left = str(int((nb[:size])) + 1)
+        if len(left) > size:
+            has_mid = True
         rev_right = ''
-        print(left, mid, '-' * (l // 2))
-        if len(mid):
-            mid = next_self_symetric[int(mid)]
-        if len(left):
-            for i , c in enumerate(left):
-                if c not in flippable_digit:
-                    left = replace(left, i, next_flippable[int(c)])
-                rev_right += symetric[int(left[i])]
+        i = 0
+        while i <  len(left):
+            if left[i] not in flippable_digit:
+                left = replace(left, i, next_flippable[int(left[i])])
+                for j in range(i + 1, size):
+                    left = replace(left, j, '0')
+            rev_right += symetric[int(left[i])]
+            i += 1
+        if has_mid:
+            rev_right = rev_right[:-1]
+        res = left + rev_right[::-1]
+        if check_if_stunning(res):
+            return res
+        return self.stun_the_middle(res)
+    
+    def stun_the_middle(self, nb: str):
+        next_self_symetric = '1888888800'
+        next_flippable     = '1255568890'   
+        symetric           = '012--59-86'
+        l = len(nb)
+        if not l % 2:
+            return nb
+        mid = l // 2
+        c = next_self_symetric[int(nb[mid])]
+        nb = replace(nb, mid, c)
+        if c == '0':
+            if l == 1:
+                return '11'
+            left_c = next_flippable[int(nb[mid - 1])]
+            nb = replace(nb, mid - 1, left_c)
+            right_c = symetric[int(left_c)]
+            nb = replace(nb, mid + 1, right_c)
+        return nb
 
-        res = left + mid + rev_right[::-1]
-        #if check_if_stunning(res):
-        return res
-        print("res", res, "stun?", check_if_stunning(res))
-
-""" incr mid if any"""
 
 @staticmethod
 def replace(s:str, idx:int, c: str):
@@ -53,19 +73,24 @@ def flip_number(nb: str) -> str:
 def check_if_stunning(nb: str) -> bool:
     if cannot_be_flipped(nb):
         return False
+    if len(nb) % 2 and nb[len(nb) // 2] not in '018':
+            return False
     if nb == flip_number(nb):
         return True
     return False
 
 
 def main():
-    # KO '9987', '10001', 99, 101
-    numbers = ['69', '161', '9987', '654321', '1260921',
-               '88888888', '123456789', '6920158510269']
-    answers = ['88', '181', '10001', '655559', '1261921',
-               '88896888', '125000521', '6920160910269']
+    """ tests """
+    numbers = ['3', '69', '161', '9987', '654321', '1260921',
+               '88888888', '123456789', '314159265359',
+               '6920158510269']
+    answers = ['8', '88', '181', '10001', '655559', '1261921',
+               '88896888', '125000521', '500000000005',
+               '6920160910269']
     result = []
     for i, number in enumerate(numbers):
+        print(" " * 50, "test", i)
         print(number)
         stunning_search = StunningNumberSearch(number)
         res = ['KO', 'OK'][answers[i] == stunning_search.next_sn]
