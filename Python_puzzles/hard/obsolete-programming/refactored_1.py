3316c0bfa38ed08dcbee8143ff9d0edce8f2a4ba
shameleon <john.mouaikel@gmail.com>
2025-01-27 23:58:20 +0100

    if else statements unsolved

--- Python_puzzles/hard/obsolete-programming/refactored_1.py
+++ Python_puzzles/hard/obsolete-programming/refactored_1.py
@@ -3,16 +3,22 @@ import sys
 class Instruction:
     def __init__(self, token):
         self.token = token
-        self.is_cond = False
+        self.is_if_fork = False
         self.child_true = None
         self.child_false = None
         self.is_end = False
+        self.is_true_branch = True
 
-    def add_children(self, instr_true, instr_false=None):
-        if instr_true:
-            self.child_true = Instruction(instr_true)
-            print(f'child to {self.token} : true {self.child_true.token}', file=sys.stderr, flush=True)
+    def add_child(self, instr_true):
+        if not instr_true:
+            instr_true = 'END'
+        self.child_true = Instruction(instr_true)
+        print(f'child to {self.token} : true {self.child_true.token}', file=sys.stderr, flush=True)
     
+    def add_else_child_to_if(self, instr_false):
+        self.child_false = Instruction(instr_false)
+        print(f'child to {self.token} : true {self.child_true.token}', file=sys.stderr, flush=True)
+
     def __repr__(self):
         return f'{self.token} T-> {self.child_true} F-> {self.child_false}\n'
 
@@ -20,17 +26,38 @@ class Instruction:
 class Definition:
     def __init__(self, def_buffer: list):
         self.name = def_buffer.pop(0)
+        self.if_fork = None
+        self.end_of_true_branch = None
+        self.is_true_branch = True
         self.head = Instruction(def_buffer.pop(0))
         self.parse_buffer(self.head, def_buffer)
 
-    def parse_buffer(self, prev, def_buffer):
-        #if prev == None:
-        #    return
+    def parse_buffer(self, current, def_buffer):
         if len(def_buffer) == 0:
-            prev.end = True
+            current.add_child('END')
             return
-        prev.add_children(def_buffer.pop(0), None)
-        self.parse_buffer(prev.child_true, def_buffer)
+        next_token = def_buffer.pop(0)
+        if next_token == 'ELSE':
+            self.is_true_branch = False
+            next_token  = def_buffer.pop(0)
+            self.if_fork.add_else_child_to_if(next_token)
+            self.end_of_true_branch = current
+            current = self.if_fork.child_false
+            self.parse_buffer(current.child_false, def_buffer)
+            return
+        elif next_token == 'FI':
+            if self.is_true_branch:
+                current.add_child(def_buffer.pop(0))
+            else:
+                self.end_of_true_branch.add_child(def_buffer.pop(0))
+                current.child_true = self.end_of_true_branch.child_true
+        else:
+            current.add_child(next_token)
+            if next_token == 'IF':
+                self.if_fork = current.child_true
+                self.is_if_fork = True    ## remove ?
+        self.parse_buffer(current.child_true, def_buffer)
+
 
     def __repr__(self):
         res = f'{self.name} :\n'
@@ -60,13 +87,8 @@ class RPN_Calculator:
             'OUT': self.out_to_stdout
         }
         self.defs = {}
-        self.is_in_cond_statement = False
-        self.if_else_buffer = []
-
 
     def implement_instruction(self, token:str) -> None:
-        if self.check_if_is_condition(token):
-            return None
         if token.lstrip('-').isdigit():
             print('push  >', token, file=sys.stderr, flush=True)
             self.stack.append(int(token))
@@ -77,8 +99,9 @@ class RPN_Calculator:
             print('def   >', token, file=sys.stderr, flush=True)
             self.implement_def(token)
         elif token == 'IF':
-            self.is_in_cond_statement = True
-            self.if_else_buffer.append(token)
+            # self.if_else_buffer.append(token)
+            # pop the top if true pick child accordingly
+            pass
         else:
             return None
         print('             ', self.stack, file=sys.stderr, flush=True)
@@ -89,34 +112,8 @@ class RPN_Calculator:
         current = definition.head
         while current != None:
             self.implement_instruction(current.token)
+            ## what if child false
             current = current.child_true
-
-    def check_if_is_condition(self, token):
-        if not self.is_in_cond_statement:
-            return False
-        self.if_else_buffer.append(token)
-        if token == 'FI':
-            self.is_in_cond_statement = False
-            self.parse_condition(bool(self.pop_nb()))
-        return True
-    
-    def parse_condition(self, cond_true: bool):
-        before_else = True
-        tmp = []
-        while len(self.if_else_buffer):
-            instr = self.if_else_buffer.pop(0)
-            if instr == 'ELSE':
-                before_else = False
-            elif instr not in ['IF', 'ELSE', 'FI']:
-                if cond_true and before_else:
-                    tmp.append(instr)
-                elif not cond_true and not before_else:
-                    tmp.append(instr)
-            elif instr == 'FI':
-                while len(self.if_else_buffer) > 0:
-                    self.is_in_cond_statement = False
-                    self.implement_instruction(self.if_else_buffer.pop())
-        print('if_else buffer = ', tmp, file=sys.stderr, flush=True)
             
     def add(self):
         if len(self.stack) >= 2:
@@ -232,19 +229,18 @@ class ObsoleteProgrammer:
     def buffer_to_definition(self):
         """ upon 'END' instruction, empties definition buffer 
         into a chained list of instructions"""
-        new_def = Definition(self.def_buffer)
-        self.rpn.defs[new_def.name] = new_def
+        if len(self.def_buffer) > 1:
+            new_def = Definition(self.def_buffer)
+            self.rpn.defs[new_def.name] = new_def
         print('defs >', self.rpn.defs, file=sys.stderr, flush=True)
 
 
 
 def main():
-    input_lines_0 = [ 'DEF PIZ SUB POS NOT',
-                   'IF SWP POP ELSE POP OVR FI POP END',
-                   '1 2 3 4 6 5 PIZ OUT'
+    input_lines = [ 'DEF PIZ OVR SUB POS IF ADD MOD ELSE MUL SUB FI SWP OUT',
+                   'END',
+                   '1 2 3 4 5 6 PIZ PIZ PIZ'
                    ]
-    input_lines = ['DEF BSH MOD SUB 4 SWP',
-                   'MUL MUL OUT END 5 6 3 2 BSH']
     obsolete = ObsoleteProgrammer()
     for line in input_lines:
         obsolete.update_with_input(line)
