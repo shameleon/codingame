bd544deb11ce422b7e3440f4d90f760494bd9f8f
shameleon <john.mouaikel@gmail.com>
2025-01-30 12:26:14 +0100

    50% success at testing. recursive tree search, memoization

--- Python_puzzles/very_hard/The-resistance/test_the_resistance.py
+++ Python_puzzles/very_hard/The-resistance/test_the_resistance.py
@@ -1,65 +1,28 @@
 import sys
-from the_resistance import Resistance, MorseTranslator
+from the_resistance import Resistance, MorseWord, MorseTranslator
 
 
 class TheResistanceTests:
-
     def test_01():
         l = '-.-'
         words = ['A', 'B', 'C', 'HELLO', 'K', 'WORLD']
-        return l, words
+        return l, words, 1
         
     def test_02():
         l = '--.-------..'
         words = ['GOD', 'GOOD', 'MORNING', 'G', 'HELLO']
-        return l, words
+        return l, words, 1
     
     def test_03():
         l = '......-...-..---.-----.-..-..-..'
         words = ['HELL', 'HELLO', 'OWORLD', 'WORLD', 'TEST']
-        return l, words
+        return l, words, 2
     
     def test_05():
         l = '-.-..---.-..---.-..--'
         words = 'CAT KIM TEXT TREM CEM'.split()
-        return l, words
+        return l, words, 125
 
-class MorseWord:
-    def __init__(self, word, word_in_morse, seq):
-        self.word = word
-        self.mord = word_in_morse
-        self.indices = self.indices_in_seq(seq)
-
-    def indices_in_seq(self, seq) -> list:
-        if self.mord == seq:
-            return [0]
-        indices = []
-        i = 0
-        while i < len(seq) - len(self.mord):
-            j = seq.find(self.mord, i, len(seq))
-            if j == -1:
-                break
-            indices.append(j)
-            i = j + 1
-        return indices
-    
-    def is_at_index(self, start_index):
-        if start_index in self.indices:
-            return start_index + len(self.mord)
-        return -1
-
-    def __repr__(self):
-        res = f'{self.word} {self.mord}'
-        if len(self.indices) > 0:
-            res += f'{self.indices}\n'
-        return res
-
-def get_mords_(morse_words, l, i):
-    p = dict(zip(range(len(l)), [] * len(l))) 
-    for mord in morse_words:
-        j = mord.is_at_index(i)
-        if j > -1:
-            
 
 def main():
     """ 
@@ -74,19 +37,19 @@ def main():
              TheResistanceTests.test_03,
              TheResistanceTests.test_05
              ]
-    translate = MorseTranslator()
+    outcomes = []
     for test_input in tests:
-        l, words = test_input()
+        l, words, answer= test_input()
         print(l, file=sys.stderr, flush=True)
         print(words, file=sys.stderr, flush=True)
-        morse_words = []
+        resist = Resistance(l)
         for word in words:
-            m = translate.morsed(word)
-            if m in l:
-                morse_words.append(MorseWord(word, m, l))
-        get_combination(l, morse_words, 0)
+            resist.add_new_input_word(word)
+        result = resist.number_of_solutions()
+        print(result)
+        outcomes.append(['KO', 'OK'][result == answer])
+    print(dict(zip(range(len(tests)), outcomes)), file=sys.stderr, flush=True)
 
-        print(morse_words)
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
--- Python_puzzles/very_hard/The-resistance/the_resistance.py
+++ Python_puzzles/very_hard/The-resistance/the_resistance.py
@@ -1,40 +1,69 @@
 import sys
-import math
-from itertools import permutations 
+import re
 
-""" progreess 10%"""
+""" progress 50% - KO test04, many validators"""
+
 class MorseTranslator:
     def __init__(self):
         self.morse_code = dict(A='.-', B='-...', C='-.-.', D='-..', E='.', F='..-.', G='--.',
-                          H='....', I='..', J='.---', K='-.-', L='.-..', M='--', N='-.',
-                          O='---', P='.--.', Q='--.-', R='.-.', S='...', T='-', U='..-',
-                          V='...-', W='.--', X='-..-', Y='-.--', Z='--..')
-    def morsed(self, word):
-        mord = ""
-        for s in word:
-            mord += self.morse_code[s]
-        return mord
+                               H='....', I='..', J='.---', K='-.-', L='.-..', M='--', N='-.',
+                               O='---', P='.--.', Q='--.-', R='.-.', S='...', T='-', U='..-',
+                               V='...-', W='.--', X='-..-', Y='-.--', Z='--..')
+
+    def to_morse(self, word):
+        return ''.join(self.morse_code[c] for c in word)
+
+
+class MorseWord:
+    def __init__(self, word, word_in_morse, seq):
+        self.word = word
+        self.mord = word_in_morse
+        self.indices = self.indices_in_seq(seq)
+
+    def indices_in_seq(self, seq) -> list:
+        return [match.start() for match in re.finditer(f'(?={self.mord})', seq)]
+    
+    def from_index_to_next_index(self, start_index):
+        if start_index in self.indices:
+            return start_index + len(self.mord)
+        return -1
+
+    def __repr__(self):
+        res = f'{self.word} {self.mord}'
+        if len(self.indices) > 0:
+            res += f'{self.indices}\n'
+        return res
 
 
 class Resistance:
-    def __init__(self, sequence, words):
+    def __init__(self, sequence: str):
         """ """
-        self.translate = MorseTranslator()
         self.seq = sequence
-        mords = [self.translate.morsed(word) for word in words]
-        matches = {idx: mord in sequence for idx, mord in enumerate(mords)}
-        self.matches = {k: v for k, v in matches.items() if v}
-        self.idx = set(self.matches.keys())
-        print(self.idx, file=sys.stderr, flush=True)
+        self.mord_set = set()
+        self.alpha = MorseTranslator()
+        self.mem = dict()
 
-    def _permutate(self):
-        permut_set = permutations(self.idx)
-        count = len(self.idx)
-        for perm in permut_set:
-            pass
+    def add_new_input_word(self, word: str):
+        mord = self.alpha.to_morse(word)
+        if mord in self.seq:
+            self.mord_set.add(MorseWord(word, mord, self.seq))
 
-        # dict for ans
-        pass
+    def get_mords_at_index(self, i):
+        if i == len(self.seq):
+            return 1
+        if i in self.mem:  # Vérifie si on a déjà calculé cet index
+            return self.mem[i]
+        total_at_index = 0
+        for mord in self.mord_set:
+            j = mord.from_index_to_next_index(i)
+            if j > -1:
+                total_at_index += self.get_mords_at_index(j)
+        self.mem[i] = total_at_index
+        return total_at_index
+
+    def number_of_solutions(self):
+        return self.get_mords_at_index(0)
+
 
 def main():
     """ 
@@ -48,10 +77,12 @@ def main():
     n = int(input())
     print(l, file=sys.stderr, flush=True)
     print(n, file=sys.stderr, flush=True)
-    words = [input() for i in range(n)]
-    decode = Resistance(l, n, words)
-    [print(word, file=sys.stderr, flush=True) for word in words]
-    print("2")
+    resist = Resistance(l)
+    for _ in range(n):
+        resist.add_new_input_word(input())
+    result = resist.number_of_solutions()
+    print(result)
+
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
